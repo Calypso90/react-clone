d18889416a338af1b9e39ff888d09a593fe18d3b
Calypso <calypsotheatre@gmail.com>
2023-11-27 16:29:42 -0500

    clone of page using react

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,41 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import alienPlanets from "./lib/planets.jsx"
+import Divider from "./components/divider.jsx";
+import Spacer from "./components/spacer.jsx";
+import Image from "next/image"
 
 export default function Home() {
+  const [planets, setPlanets] = useState(alienPlanets);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <p>1</p>
+      <Divider />
+      <div className = "blueSquare"></div>
+      <Spacer />
+      <p>2</p>
+      <Divider />
+      <div>
+        <Image src={"/pingu.png"} width={500} height={500} />
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <Spacer />
+      <p>3</p>
+      <Divider />
+      <h3>Alien Planets</h3>
+      <div className="planets-container">
+      {planets.map((planet) => {
+        return (
+            <div className="planet-info">
+              <p className="title">{planet.emoji}</p>
+              <p className="title">{planet.name}</p>
+              <p>{planet.name} is {planet.distanceFromEarth} from earth. {planet.name} is believed to 
+              {planet.hasLife === true ? ' have life. ' : ' not have life. '}
+              The atmosphere of {planet.name} is {planet.atmosphere}.</p>
+            </div>
+        );
+      })}
       </div>
     </main>
-  )
+  );
 }
